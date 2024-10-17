9f55c6b2edada756ce643f788dcd4fae6cf74fad
zero3062 <esung246@gmail.com>
2024-10-17 21:00:50 +0900

    fix: setting(prod)

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -6,8 +6,13 @@ import { RoundINF, SheetNmINF } from '../../types/types';
 import useAuth from '../../hooks/useAuth';
 import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:8080');
-// const socket = io.connect('https://eternal-return-league-server.vercel.app/');
+// const socket = io.connect('http://localhost:8080', {
+//   transports: ['websocket'],
+//});
+
+const socket = io.connect('https://eternal-return-league-server.vercel.app/', {
+  transports: ['websocket'],
+});
 
 const Main = () => {
   const [open, setOpen] = useState(false);
